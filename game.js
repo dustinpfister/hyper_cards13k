bb698f9fda52f6fa206ef1ca7447f48b278de493
dustinpfister <dustin.pfister@gmail.com>
2018-08-18 10:29:34 -0400

    card,deck, and match classes staretd

--- game.js
+++ game.js
@@ -1,20 +1,27 @@
 (function () {
 
     // CANVAS
+
+    // creating canvas, and getting a reference to the
+    // 2d drawing context
     var canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d');
 
+    // seting css via style api
     canvas.width = 320;
     canvas.height = 240;
-	canvas.style.position = 'absolute';
+    canvas.style.position = 'absolute';
 
+    // append to body
     document.body.appendChild(canvas);
 
+    // scale canvas helper
     var scale = function () {
         canvas.style.width = (window.innerWidth) + 'px';
         canvas.style.height = (window.innerHeight) + 'px';
     };
 
+    // clear screen helper
     var cls = function () {
 
         ctx.fillStyle = 'balck';
@@ -22,8 +29,44 @@
 
     };
 
+    // scale from first time, and do so on each resize
     scale();
+    window.addEventListener('resize', function () {
+
+        scale();
+
+    })
+
+    // cls for first time
     cls();
+
+    // CARD CLASS
+    var Card = function (opt) {
+
+        this.attack = opt.attack;
+        this.hpMax = opt.hpMax;
+        this.hp = this.hpMax;
+
+    };
+
+    // DECK CLASS
+    var Deck = function () {
+
+        // faction index (player or ai deck)
+        this.faction = opt.faction; // 0=player 1=ai
+
+        // the cards in the deck
+        this.cards = [];
+
+    };
+
+    // MATCH CLASS
+    var Match = function () {
+
+        this.turn = 0;
+        this.curFact = 0; // current faction index
+
+    }
 
 }
     ());
